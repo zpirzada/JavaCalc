643cda5bd3d1cf67507ea1ba6d4ca6171c3859d8
Zeb Pirzada <zpirzada@gmail.com>
2022-12-29 21:49:10 +0100

    New method for addition with 2 values

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
+
+    static int myAdd(int x, int y){
+        return x + y;
+
+    }
     public static void main(String[] args) {
+        int x = 2;
+        int y =5;
+
         System.out.println("Hello world from Zeb for Java 2 !");
+
+        System.out.println("As per the addition method x + y = " + myAdd(x,y) );
     }
 }
